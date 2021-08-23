1b1d298d27f288dcd74ae96142f75aaa58f42a88
DeanLee <deanlee1201@hotmail.com>
2021-08-23 14:07:01 +0800

    config: change mock data base

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ import { ACCESS_TOKEN } from '@/store/mutation-types'
 const request = axios.create({
   // API 请求的默认前缀
   // baseURL: process.env.VUE_APP_API_BASE_URL,
-  baseURL: 'http://rap2.qdum.com:38080/app/mock/24',
+  baseURL: 'http://rap2api.taobao.org/app/mock/289321',
   timeout: 6000, // 请求超时时间
 })
 
